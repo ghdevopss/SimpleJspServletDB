76a58d859c1814985437c5d70f7dcaf6c681a4c8
Sowmya Mohanan <sowmya.mohanan@cognizant.com>
2018-04-19 12:11:42 +0000

    testadd

---
+++ src/emailDemo/Testadd.java
@@ -0,0 +1,19 @@
+package emailDemo;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import junit.framework.Assert;
+
+public class Testadd {
+
+	@Test
+	public void testadd()
+	{
+		Addition obj=new Addition(3,5);
+		System.out.println("inside this method");
+		Assert.assertEquals(8,obj.addition());
+	}
+
+}
